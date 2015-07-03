7ae36dd06117601d434676bc43e768c407b7506b
pramod.kp <pramod.kp@experionglobal.com>
2015-07-03 11:48:28 +0530

    add log

--- app/src/androidTest/java/com/example/pramodkp/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/pramodkp/myapplication/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.example.pramodkp.myapplication;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,6 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        Log.d("test", "test");
     }
 }
